7db0e3149f4a6c279a1865649b3e00d0824ccd6d
Marcin Kotlicki <kotlickimarcin@gmail.com>
2023-02-23 22:29:58 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
+import { Day1, Day2, Day3, Spinner1 } from "./challenges/100-days-css/index.t";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 32,
+                overflow: "auto",
+                position: "relative",
+                height: "100vh",
+                padding: 128
+            }}
+        >
+            <h1>Ayron&apos;s Web Portfolio</h1>
+            <div style={{ position: "relative", minHeight: 400 }}>
+                <Day1 />
+            </div>
+            <div style={{ position: "relative", minHeight: 400 }}>
+                <Day2 />
+            </div>
+            <div style={{ position: "relative", minHeight: 400 }}>
+                <Day3 />
+            </div>
+            <div style={{ position: "relative", minHeight: 400 }}>
+                <Spinner1 />
+            </div>
         </div>
     );
 }
