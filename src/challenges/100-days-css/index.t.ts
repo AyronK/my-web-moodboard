7164604a0144e0b300d5ab6057a1c32451dd8520
Marcin Kotlicki <26150059+AyronK@users.noreply.github.com>
2023-02-23 22:30:21 +0100

    Feature/present work so far (#2)
    * Moved elements to SB
    
    * Added linter and prettier
    
    * Added husky
    
    * Test
    
    * Cleanup

---
+++ src/challenges/100-days-css/index.t.ts
@@ -0,0 +1,6 @@
+import { Frame as Day1 } from "./001/source";
+import { Frame as Day2 } from "./002/source";
+import { Frame as Day3 } from "./003/source";
+import { Frame as Spinner1 } from "./loaders/001/source";
+
+export { Day1, Day2, Day3, Spinner1 };
---
+++ src/challenges/100-days-css/loaders/001/source.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./source.css";
+
+export const Frame = () => (
+    <div className="frame">
+        <div className="center">
+            <div className="loader"></div>
+        </div>
+    </div>
+);
