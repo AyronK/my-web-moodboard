87848bc12174302de68f89672eda3ae6d5e4fefe
Marcin Kotlicki <kotlickimarcin@gmail.com>
2023-02-25 00:20:29 +0100

    Start

--- src/challenges/100-days-css/index.t.ts
+++ src/challenges/100-days-css/index.t.ts
@@ -3,5 +3,6 @@ import { Day2 } from "./002/source";
 import { Day3 } from "./003/source";
 import { Day4 } from "./004/source";
 import { Day5 } from "./005/source";
+import { Day6 } from "./006/source";
 
-export { Day1, Day2, Day3, Day4, Day5 };
+export { Day1, Day2, Day3, Day4, Day5, Day6 };
