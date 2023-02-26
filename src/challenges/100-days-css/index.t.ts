9fd786627f5a38a4a450a70b135ae62405d95894
Marcin Kotlicki <26150059+AyronK@users.noreply.github.com>
2023-02-26 13:17:09 +0100

    Day 7 (#9)

--- src/challenges/100-days-css/index.t.ts
+++ src/challenges/100-days-css/index.t.ts
@@ -4,5 +4,6 @@ import { Day3 } from "./003/source";
 import { Day4 } from "./004/source";
 import { Day5 } from "./005/source";
 import { Day6 } from "./006/source";
+import { Day7 } from "./007/source";
 
-export { Day1, Day2, Day3, Day4, Day5, Day6 };
+export { Day1, Day2, Day3, Day4, Day5, Day6, Day7 };
