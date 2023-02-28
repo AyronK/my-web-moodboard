56b3c206257f7028e88479bc9d4da4d7ff83cbf1
Marcin Kotlicki <kotlickimarcin@gmail.com>
2023-02-28 16:41:10 +0100

    Story for demo

--- src/challenges/100-days-css/009/source.stories.tsx
+++ src/challenges/100-days-css/009/source.stories.tsx
@@ -1,5 +1,5 @@
-import { ComponentMeta } from "@storybook/react";
-import React from "react";
+import { ComponentMeta, Story } from "@storybook/react";
+import React, { useEffect, useRef, useState } from "react";
 import { Frame } from "../../../Frame/Frame";
 import { Day9 as Component, Weather } from "./source";
 
@@ -80,5 +80,40 @@ export const NightSnowing = {
     args: {
         ...Night.args,
         weather: Weather.Snowing
+    }
+};
+
+export const Demo = {
+    decorators: [
+        (Story: Story, args: any) => {
+            const frames = useRef([
+                { weather: Weather.Cloudy, night: true },
+                { weather: Weather.Cloudy, night: false },
+                { weather: Weather.Snowing, night: true },
+                { weather: Weather.Snowing, night: false },
+                { weather: Weather.Raining, night: true },
+                { weather: Weather.Raining, night: false },
+                { weather: Weather.Storm, night: true },
+                { weather: Weather.Storm, night: false },
+                { weather: Weather.ClearSky, night: true },
+                { weather: Weather.ClearSky, night: false }
+            ]);
+            const [newArgs, setNewArgs] = useState(frames.current[0]);
+
+            useEffect(() => {
+                let i = 0;
+                const timer = setInterval(() => {
+                    i++;
+                    setNewArgs(frames.current[i % frames.current.length]);
+                }, (20 * 1000) / frames.current.length);
+                return () => clearInterval(timer);
+            }, []);
+
+            return <Story args={newArgs} />;
+        }
+    ],
+    argTypes: {
+        weather: { control: false },
+        night: { control: false }
     }
 };
