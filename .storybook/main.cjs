3ae8dd5fa2ad39902aa8463ca18ce037582b88db
Marcin Kotlicki <kotlickimarcin@gmail.com>
2023-03-14 00:27:44 +0100

    Migrated to Vite and SB 7
    Lock update
    Updated stack blitz config
    
    Updated stackblitz config
    
    3 files modified
    
    3 files modified
    
    Updated locks
    Updated lock
    Lock
    2 files modified
    
    Locks
    Fix
    Fixes
    Removed
    SB working on stacklitz and npm/turbo
    
    2 files modified
    
    Migration to vite and SB 7
    1 file modified
    
    Fixed publish dir

---
+++ .storybook/main.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  features: {
+    interactionsDebugger: true
+  },
+  docs: {
+    autodocs: true
+  }
+};
