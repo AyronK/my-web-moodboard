e8db85ebe969ae57a336587d1d9f11ca988f98dd
AyronK <26150059+AyronK@users.noreply.github.com>
2023-03-19 20:18:02 +0100

    Day28

---
+++ src/challenges/100-days-css/028/source.tsx
@@ -0,0 +1,15 @@
+import { Bell } from "phosphor-react";
+import React from "react";
+import styles from "./source.module.scss";
+
+export const Day28: React.FC = () => {
+    return (
+        <div className={styles.bellContainer}>
+            <div className={styles.wobble} />
+            <div className={styles.wobble} />
+            <div className={styles.wobble} />
+            <div className={styles.wobble} />
+            <Bell className={styles.bell} size={64} weight="fill" />
+        </div>
+    );
+};
