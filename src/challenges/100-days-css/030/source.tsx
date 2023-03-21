e33b854051dc1687bf3f25ecc47413c8507065e3
AyronK <26150059+AyronK@users.noreply.github.com>
2023-03-21 15:31:05 +0100

    celtic knot with animation

---
+++ src/challenges/100-days-css/030/source.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./source.module.scss";
+
+export const Day30: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <svg>
+                <path d="M 200,-60 200,145 Q 260,250 140,250 L 140,250 0,400"></path>
+                <path d="M 0,400 140,250 140,250 Q 200,145 260,250 L 260,250 400,400"></path>
+                <path d="M 400,400 260,250 Q 140,250 200,145 L 200,145 200,0"></path>
+                {/* <circle r="52" cx="200" cy="214"/> */}
+                {/* <path d="M 200,145 Q 200,217 140,250"></path>
+                <path d="M 140,250 Q 200,217 260,250"></path>
+                <path d="M 260,250 Q 200,217 200,145"></path> */}
+                {/* 
+                <path d="M 140,250 200,145 260,250 140,250"></path> */}
+            </svg>
+        </div>
+    );
+};
