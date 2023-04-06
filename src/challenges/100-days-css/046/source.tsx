1cddc7891f7744378f9983acea3ea5d4d906a1aa
AyronK <26150059+AyronK@users.noreply.github.com>
2023-04-06 20:50:41 +0200

    wip

--- src/challenges/100-days-css/046/source.tsx
+++ src/challenges/100-days-css/046/source.tsx
@@ -3,5 +3,15 @@ import { Center } from "../../../Frame/Frame";
 import styles from "./source.module.scss";
 
 export const Day46: React.FC = () => {
-    return <Center></Center>;
+    return (
+        <Center>
+            {Array.from({ length: 6 }).map((_, i) => (
+                <div key={i} className={styles.ring}>
+                    {Array.from({ length: 12 }).map((_, i) => (
+                        <div key={i} className={styles.dot} />
+                    ))}
+                </div>
+            ))}
+        </Center>
+    );
 };
