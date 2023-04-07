a9e9f7ce7eda3c20b0d051ab3fc7bc158fe171f3
AyronK <26150059+AyronK@users.noreply.github.com>
2023-04-07 18:56:11 +0200

    wip

--- src/challenges/100-days-css/047/source.tsx
+++ src/challenges/100-days-css/047/source.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import styles from "./source.module.scss";
 
 export const Day47: React.FC = () => {
-    return <div className={styles.board}></div>;
+    return (
+        <div className={styles.board}>
+            {Array.from({ length: 20 * 20 }).map((_, i) => (
+                <input className={styles.tile} type="checkbox" />
+            ))}
+        </div>
+    );
 };
