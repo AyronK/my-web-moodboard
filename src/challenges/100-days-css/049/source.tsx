0994f50962e09e9c6c9f6e1ea9d028af969e9f0b
AyronK <26150059+AyronK@users.noreply.github.com>
2023-04-11 23:01:52 +0200

    wip

--- src/challenges/100-days-css/049/source.tsx
+++ src/challenges/100-days-css/049/source.tsx
@@ -4,7 +4,7 @@ import styles from "./source.module.scss";
 export const Day49: React.FC = () => {
     return (
         <div className={styles.board}>
-            {Array.from({ length: 50 * 50 }).map((_, i) => (
+            {Array.from({ length: 20 * 20 }).map((_, i) => (
                 <div />
             ))}
             <div className={styles.pulsar} />
