f53c446d39c2b88e48b73b14b1a71ede9380a861
AyronK <26150059+AyronK@users.noreply.github.com>
2023-04-11 21:18:01 +0200

    init

---
+++ src/challenges/100-days-css/049/source.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./source.module.scss";
+
+export const Day49: React.FC = () => {
+    return (
+        <div className={styles.board}>
+            {Array.from({ length: 50 * 50 }).map((_, i) => (
+                <div />
+            ))}
+        </div>
+    );
+};
