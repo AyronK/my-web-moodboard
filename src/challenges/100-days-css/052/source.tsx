3887641dcdede02c3ffe8b2be67db2a55b3635d1
AyronK <26150059+AyronK@users.noreply.github.com>
2023-04-17 16:08:19 +0200

    fix

--- src/challenges/100-days-css/052/source.tsx
+++ src/challenges/100-days-css/052/source.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import styles from "./source.module.scss";
 
 export const Day52: React.FC = () => {
-    return Array.from({ length: 10 }).map((_, i) => (
-        <div key={i} className={styles.circle}>
-            {Array.from({ length: 120 }).map((_, j) => (
-                <div key={j} className={styles.dot} />
+    return (
+        <>
+            {Array.from({ length: 10 }).map((_, i) => (
+                <div key={i} className={styles.circle}>
+                    {Array.from({ length: 120 }).map((_, j) => (
+                        <div key={j} className={styles.dot} />
+                    ))}
+                </div>
             ))}
-        </div>
-    ));
+        </>
+    );
 };
