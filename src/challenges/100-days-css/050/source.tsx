53a2cd83d0f33e111e6760f5c7b35446df10b1cd
AyronK <26150059+AyronK@users.noreply.github.com>
2023-04-17 12:01:20 +0200

    bounce

--- src/challenges/100-days-css/050/source.tsx
+++ src/challenges/100-days-css/050/source.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { Center } from "../../../Frame/Frame";
 import styles from "./source.module.scss";
 
 export const Day50: React.FC = () => {
-    return <div className={""}></div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.x}>
+                <div className={styles.xScale}>
+                    <div className={styles.y}>
+                        <div className={styles.yScale}>
+                            <div className={styles.ball}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
