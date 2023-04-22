e09d54fc2cca43d76be74aeca4aff8538ff73cc5
AyronK <26150059+AyronK@users.noreply.github.com>
2023-04-22 19:35:49 +0200

    init 2

---
+++ src/challenges/100-days-css/053/source.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Center } from "../../../Frame/Frame";
+import styles from "./source.module.scss";
+
+export const Day53: React.FC = () => {
+    return <Center></Center>;
+};
