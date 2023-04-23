d5d54ec9f3c0230248de0fcc66e59a20fe499698
AyronK <26150059+AyronK@users.noreply.github.com>
2023-04-23 19:29:44 +0200

    wip

--- src/challenges/100-days-css/055/source.tsx
+++ src/challenges/100-days-css/055/source.tsx
@@ -3,5 +3,12 @@ import { Center } from "../../../Frame/Frame";
 import styles from "./source.module.scss";
 
 export const Day55: React.FC = () => {
-    return <Center></Center>;
+    return (
+        <Center>
+            <div className={styles.thermostat}>
+                <div className={styles.border}></div>
+                <div className={styles.temperature}></div>
+            </div>
+        </Center>
+    );
 };
