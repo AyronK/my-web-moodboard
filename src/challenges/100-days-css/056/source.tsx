d2510a4939f32c289a515d75accb8edb638f0469
AyronK <26150059+AyronK@users.noreply.github.com>
2023-04-29 19:13:30 +0200

    wip

--- src/challenges/100-days-css/056/source.tsx
+++ src/challenges/100-days-css/056/source.tsx
@@ -3,5 +3,13 @@ import { Center } from "../../../Frame/Frame";
 import styles from "./source.module.scss";
 
 export const Day56: React.FC = () => {
-    return <Center></Center>;
+    return (
+        <Center>
+            <div className={styles.flower}>
+                {Array.from({ length: 15 }).map((_, i) => (
+                    <div className={styles.petal} key={i} />
+                ))}
+            </div>
+        </Center>
+    );
 };
