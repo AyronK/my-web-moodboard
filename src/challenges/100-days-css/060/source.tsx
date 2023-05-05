4e490c4f163ab3cd02ca80b9d172842dce8b2ecf
AyronK <26150059+AyronK@users.noreply.github.com>
2023-05-05 14:33:05 +0200

    init

---
+++ src/challenges/100-days-css/060/source.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Center } from "../../../Frame/Frame";
+import styles from "./source.module.scss";
+
+export const Day60: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.overlay}></div>
+            <Center>
+                <div className={styles.text}>Hover</div>
+            </Center>
+        </div>
+    );
+};
