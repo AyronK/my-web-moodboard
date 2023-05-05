2db465a91d16caf5fe8e5f4b49771d01bd55c9f0
AyronK <26150059+AyronK@users.noreply.github.com>
2023-05-05 13:35:17 +0200

    wip

--- src/challenges/100-days-css/059/source.tsx
+++ src/challenges/100-days-css/059/source.tsx
@@ -2,5 +2,15 @@ import React from "react";
 import styles from "./source.module.scss";
 
 export const Day59: React.FC = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div className={styles.image}></div>
+            <div className={styles.image}>
+                {Array.from({ length: 8 }).map((_, i) => (
+                    <div key={i} className={styles.stripe}></div>
+                ))}
+            </div>
+            <div className={styles.text}>Slice transition</div>
+        </div>
+    );
 };
