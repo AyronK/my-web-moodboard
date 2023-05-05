67acb32801c9d5004aeb5883d6d000dfa19fb24e
AyronK <26150059+AyronK@users.noreply.github.com>
2023-05-05 15:55:59 +0200

    wip

--- src/challenges/100-days-css/060/source.tsx
+++ src/challenges/100-days-css/060/source.tsx
@@ -5,6 +5,12 @@ import styles from "./source.module.scss";
 export const Day60: React.FC = () => {
     return (
         <div className={styles.container}>
+            {Array.from({ length: 200 }).map((_, i) => (
+                <div className={styles.trigger} key={i}></div>
+            ))}
+            <Center>
+                <div className={styles.text}>Hover</div>
+            </Center>
             <div className={styles.overlay}>
                 <Center>
                     <div className={styles.text}>Hover</div>
