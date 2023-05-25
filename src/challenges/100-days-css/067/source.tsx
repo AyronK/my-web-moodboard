a7f466c0637eadd9e504498d23bb5f2fa84213a7
AyronK <26150059+AyronK@users.noreply.github.com>
2023-05-25 15:51:49 +0200

    day67

--- src/challenges/100-days-css/067/source.tsx
+++ src/challenges/100-days-css/067/source.tsx
@@ -1,6 +1,24 @@
 import React from "react";
+
 import styles from "./source.module.scss";
 
 export const Day67: React.FC = () => {
-    return <div className={styles.container}></div>;
+    return (
+        <div className={styles.umbrella}>
+            <div className={styles.pole} />
+            <div className={styles.poleShadow} />
+            <div className={styles.top}>
+                {Array.from({ length: 12 }).map((_, i) => (
+                    <div key={i} className={styles.triangle} />
+                ))}
+            </div>
+            <div className={styles.topShadow}>
+                <div className={styles.shadow}>
+                    {Array.from({ length: 12 }).map((_, i) => (
+                        <div key={i} className={styles.triangle} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
