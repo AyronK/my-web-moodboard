78b823daba8f6d33ad4a91f07c4046fa2077dba7
AyronK <26150059+AyronK@users.noreply.github.com>
2023-06-19 21:50:46 +0200

    3 files modified

--- src/challenges/100-days-css/070/source.tsx
+++ src/challenges/100-days-css/070/source.tsx
@@ -1,5 +1,32 @@
 import styles from "./source.module.scss";
 
 export const Day70: React.FC = () => {
-    return <div className={styles.container}></div>;
+    return (
+        <div className={styles.calendar}>
+            <div className={styles.day}>
+                <div>
+                    <span>Monday</span>
+                    <span>25</span>
+                </div>
+            </div>
+            <div className={styles.day}>
+                <div>
+                    <span>Tuesday</span>
+                    <span>26</span>
+                </div>
+            </div>
+            <div className={styles.day}>
+                <div>
+                    <span>Wednesday</span>
+                    <span>27</span>
+                </div>
+            </div>
+            <div className={styles.day}>
+                <div>
+                    <span>Thursday</span>
+                    <span>28</span>
+                </div>
+            </div>
+        </div>
+    );
 };
