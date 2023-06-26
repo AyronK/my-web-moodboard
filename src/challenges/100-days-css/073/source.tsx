187b6ce86f694be81e09a307e86cb1961f57d1ef
Marcin Kotlicki <26150059+AyronK@users.noreply.github.com>
2023-06-26 17:22:36 +0200

    Day73 (#67)
    * Init 72
    
    * 4 files modified
    
    * init73
    
    * 2 files modified
    
    * 2 files modified

---
+++ src/challenges/100-days-css/073/source.tsx
@@ -0,0 +1,19 @@
+import { Center } from "../../../Frame/Frame";
+import styles from "./source.module.scss";
+
+export const Day73: React.FC = () => {
+    return (
+        <Center>
+            <div className={styles.text}>
+                Time&nbsp;for&nbsp;a
+                <div>
+                    <span>break</span>
+                    <span>coffee</span>
+                    <span>cookie</span>
+                    <span>smile</span>
+                </div>
+                .
+            </div>
+        </Center>
+    );
+};
