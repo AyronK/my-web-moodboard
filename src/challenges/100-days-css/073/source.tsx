a0b0b63587d5d53b4d69dc3d23743f91f4d9980b
AyronK <26150059+AyronK@users.noreply.github.com>
2023-06-26 17:03:11 +0200

    2 files modified

--- src/challenges/100-days-css/073/source.tsx
+++ src/challenges/100-days-css/073/source.tsx
@@ -2,5 +2,17 @@ import { Center } from "../../../Frame/Frame";
 import styles from "./source.module.scss";
 
 export const Day73: React.FC = () => {
-    return <Center></Center>;
+    return (
+        <Center>
+            <div className={styles.text}>
+                I'm a&nbsp;
+                <div>
+                    <span>Leopard</span>
+                    <span>Tiger</span>
+                    <span>Panther</span>
+                    <span>Jaguar</span>
+                </div>
+            </div>
+        </Center>
+    );
 };
