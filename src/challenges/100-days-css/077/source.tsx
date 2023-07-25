21bf05cd0e11dc1110cc8af4fd5d91ac4d138b60
AyronK <kotlickimarcin@gmail.com>
2023-07-25 13:44:18 +0200

    improvements

--- src/challenges/100-days-css/077/source.tsx
+++ src/challenges/100-days-css/077/source.tsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react"
 import styles from "./source.module.scss";
 
 export const Day77: React.FC = () => {
+    const [number, setNumber] = useState(8);
+    useEffect(() => {
+        if(number === 0){
+            setNumber(8)
+        }
+    }, [number])
     return (
         <div className={styles.container}>
-            {Array.from({length: 5}).map((_, i) => <div key={i} className={styles.ball} />)}            
+            <button className={styles.button} onClick={() => setNumber(n => n === 8 ? 1 : 0)}>{number === 5 ? "Disable blur" : "Enable blur"}</button>
+            {Array.from({length: number}).map((_, i) => <div key={i} className={styles.ball} />)}            
         </div> 
     );
 };
