1d4308be8a2f37e34b7c6a2b3b2cf3843016a8da
AyronK <kotlickimarcin@gmail.com>
2023-07-25 13:52:58 +0200

    Fixed button for motion blur

--- src/challenges/100-days-css/077/source.tsx
+++ src/challenges/100-days-css/077/source.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react"
 import styles from "./source.module.scss";
 
+const numberOfBalls = 8;
+
 export const Day77: React.FC = () => {
-    const [number, setNumber] = useState(8);
+    const [number, setNumber] = useState(numberOfBalls);
     useEffect(() => {
         if(number === 0){
-            setNumber(8)
+            setNumber(numberOfBalls)
         }
     }, [number])
     return (
         <div className={styles.container}>
-            <button className={styles.button} onClick={() => setNumber(n => n === 8 ? 1 : 0)}>{number === 5 ? "Disable blur" : "Enable blur"}</button>
+            <button className={styles.button} onClick={() => setNumber(n => n === numberOfBalls ? 1 : 0)}>{number === numberOfBalls ? "Disable blur" : "Enable blur"}</button>
             {Array.from({length: number}).map((_, i) => <div key={i} className={styles.ball} />)}            
         </div> 
     );
