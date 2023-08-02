d3a7a3fa7c9058c9b92ed3ee35ce4e9e98cd38c1
AyronK <kotlickimarcin@gmail.com>
2023-08-02 21:52:09 +0200

    wip

--- src/challenges/100-days-css/079/source.tsx
+++ src/challenges/100-days-css/079/source.tsx
@@ -1,11 +1,12 @@
+import { Center } from "../../../Frame/Frame";
 import styles from "./source.module.scss";
 
 export const Day79: React.FC = () => {
     return (
-        <div>
+        <Center>
             {Array.from({ length: 10 }).map((_, i) => 
                 <div key={i} className={styles.line}/>
             )}
-        </div> 
+        </Center> 
     );
 };
