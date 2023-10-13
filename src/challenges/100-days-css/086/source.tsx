41b81f0d17383989ae0f4d3bdc6c7180043a0120
AyronK <kotlickimarcin@gmail.com>
2023-10-13 16:26:29 +0200

    day86

--- src/challenges/100-days-css/086/source.tsx
+++ src/challenges/100-days-css/086/source.tsx
@@ -4,7 +4,13 @@ import styles from "./source.module.scss";
 export const Day86: React.FC = () => {
     return (
         <Center>
-            
+            <div className={styles.cradle}>
+                <div className={styles.ball}/>
+                <div className={styles.ball}/>
+                <div className={styles.ball}/>
+                <div className={styles.ball}/>
+                <div className={styles.ball}/>
+            </div>
         </Center>
     );
 };
