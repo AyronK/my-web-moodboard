f67d9b663307d0edb79617ba74f35d2116c25889
AyronK <kotlickimarcin@gmail.com>
2023-11-24 10:52:35 +0100

    init day 91

---
+++ src/challenges/100-days-css/091/source.tsx
@@ -0,0 +1,11 @@
+import { Center } from "../../../Frame/Frame";
+import styles from "./source.module.scss";
+
+export const Day91: React.FC = () => {
+    return (
+        <Center>
+            <div></div>
+        </Center>
+    );
+};
+
