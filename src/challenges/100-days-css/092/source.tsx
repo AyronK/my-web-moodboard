2b2f58bd202c27cd3324f66a967dfc6828452a4a
AyronK <kotlickimarcin@gmail.com>
2023-11-29 10:45:24 +0100

    fixed day 92

---
+++ src/challenges/100-days-css/092/source.tsx
@@ -0,0 +1,9 @@
+import styles from "./source.module.scss";
+
+export const Day92: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            {Array.from({ length: 16 }).map((_, i) => <div className={styles.point} key={i} />)}
+        </div>
+    );
+};
