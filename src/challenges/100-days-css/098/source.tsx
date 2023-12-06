e20b3fc4ef433315a9be4f864a5732f8f512d73c
AyronK <kotlickimarcin@gmail.com>
2023-12-06 11:45:39 +0100

    wip

--- src/challenges/100-days-css/098/source.tsx
+++ src/challenges/100-days-css/098/source.tsx
@@ -3,6 +3,12 @@ import styles from "./source.module.scss";
 export const Day98: React.FC = () => {
     return (
         <div className={styles.container}>
+            <div className={styles.box}>
+                <div className={styles.top} />
+                <div className={styles.bottom} />
+                <div className={styles.right} />
+                <div className={styles.left} />
+            </div>
         </div>
     );
 };
