ec0942db15ee8427d05d11c7d74b902b8472a030
Marcin Kotlicki <26150059+AyronK@users.noreply.github.com>
2024-06-08 21:29:36 +0000

    Fixed url and base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/my-web-moodboard/",
+    base: "/",
     server: {
         port: 8000
     },
